29af38b8e409e3d8a542f21192e6cf74298c882d
vinaykadam115 <vinaykadam23030@gmail.com>
2022-07-13 11:13:10 +0530

    i am uploading this files

---
+++ src/logicalPrograms/ReverseString.java
@@ -0,0 +1,60 @@
+package logicalPrograms;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc=new Scanner(System.in);    System.out.println("please enter number");
+		int a = sc.nextInt();
+		
+//		int a=400055;
+		String b = Integer.toString(a);
+		
+		int zeroes = 0 ;
+		for(int i=0;i<=b.length()-1;i++)
+		{
+		   if (b.charAt(i)=='0')
+		   {
+			   zeroes++;
+		   }
+		
+		}
+			System.out.print("Number of zeroes in giver number: "+zeroes);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		String a= "GiT";
+//		String b="";
+//		for(int i=a.length()-1;i>=0;i--)//T
+//		{
+//			b=b+a.charAt(i);
+//		}
+//		System.out.println(b+" is reversed string");
+	
+//		int a=20004;
+//		String b = Integer.toString(a);
+//		String finalb = "";
+//		    for(int i=b.length()-1;i>=0;i--)
+//		    {
+//		    	finalb=finalb+b.charAt(i);
+//		    }
+//		int finala = Integer.parseInt(finalb);    
+//		    
+//		   System.out.println(finala); 
+		    
+	}
+
+}
